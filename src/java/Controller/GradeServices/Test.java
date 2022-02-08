8814cb775da21e4df28d821392d2e21dec21ec6f
Altamish <84109105+iplayer786@users.noreply.github.com>
2022-02-07 23:34:56 -0700

    Test Class for Git Issues
    Wrote a simple class with a comment, trying to resolve an issue with IDE's. Please delete this change or class later on.

---
+++ src/java/Controller/GradeServices/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.GradeServices;
+
+/**
+ * altamish wrote this, did Seulgi receive this?
+ * @author Administrator
+ */
+public class Test {
+    
+}
