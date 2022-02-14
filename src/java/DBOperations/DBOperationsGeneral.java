c84b8c3c651a54c3dd1f385672ceaea634836212
massvm <massvm@massvm-VirtualBox>
2022-02-14 12:00:15 -0700

    Added a new package DBOperations to  source packages. Within this package I have added the DBOperationsGeneral class which will hold general DBOpertaions methods that will be used within all DBOperations classes

--- src/java/DBOperations/DBOperationsGeneral.java
+++ src/java/DBOperations/DBOperationsGeneral.java
@@ -5,10 +5,33 @@
  */
 package DBOperations;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
- * @author massvm
+ * @author RyanChecora
+ * I have created the DBOperationsGeneral class to handle all general database operations. At this point i believe  that we will
+ * create other more specific dbOperations within other specific db classes (i.e. grade db operations, announcement db operations)
+ * to allow for greater organization.
+ * 
  */
 public class DBOperationsGeneral {
+    
+    private Connection getConnection(){
+        Connection conn = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/massupdated","admin","password");
+        }
+        catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        catch(ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+        return conn;
+    }
     
 }
