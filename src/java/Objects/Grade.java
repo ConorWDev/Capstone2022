3eaad449734f233cc28f0677539a3b28f1172907
massvm <massvm@massvm-VirtualBox>
2022-02-14 13:12:27 -0700

    Working on reportcard page as well as course grade page

--- src/java/Objects/Grade.java
+++ src/java/Objects/Grade.java
@@ -5,12 +5,80 @@
  */
 package Objects;
 
+import DBOperations.DBOperationsGrade;
+
 /**
  *
  * @author massvm
  */
 public class Grade {
     
+    DBOperationsGrade dbOps = new DBOperationsGrade();
     
+    private String studentName;
+    private String assignmentName;
+    
+    //TODO add lessonName courseName attributes via further DB queries
+    //private String lessonName;
+    //private String courseName;
+    
+    private double mark;
+    
+    private boolean isVisible;
+    
+    
+    public Grade(String studentUsername, String assignmentID, String mark, String isVisible){
+        
+        this.studentName =  dbOps.getName(studentUsername);
+        this.assignmentName = dbOps.getAssignmentName(assignmentID);
+        
+        this.mark = Double.parseDouble(mark);
+        if (isVisible.equalsIgnoreCase("Y")){
+            this.isVisible = true;
+        }
+        else{
+            this.isVisible = false;
+        }
+    }
+
+    public TestDBOperations getDbOps() {
+        return dbOps;
+    }
+
+    public void setDbOps(TestDBOperations dbOps) {
+        this.dbOps = dbOps;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getAssignmentName() {
+        return assignmentName;
+    }
+
+    public void setAssignmentName(String assignmentName) {
+        this.assignmentName = assignmentName;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+    public boolean isIsVisible() {
+        return isVisible;
+    }
+
+    public void setIsVisible(boolean isVisible) {
+        this.isVisible = isVisible;
+    }
     
 }
