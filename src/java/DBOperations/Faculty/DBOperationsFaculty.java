ca482c582c8358f400ef8a21bc9e42abeab39184
massvm <massvm@massvm-VirtualBox>
2022-03-08 14:37:30 -0700

    DBOperationsFaculty added

---
+++ src/java/DBOperations/Faculty/DBOperationsFaculty.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBOperations.Faculty;
+
+import DBOperations.ConnectionPool;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author massvm
+ */
+public class DBOperationsFaculty {
+    
+    //get faculty name from username
+     public String getFacultyName(String facultyUsername){
+        String result ="";
+        String sql = "select first_name, last_name from ma_faculty where username = ?;";
+        ConnectionPool cp = ConnectionPool.getInstance();
+        
+         try{
+            
+            Connection conn = cp.getConnection();
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, facultyUsername);
+            ResultSet rs = st.executeQuery();
+            
+            while (rs.next()){
+                result = rs.getString(1) + " " + rs.getString(2);
+            }
+            
+            st.close();
+            rs.close();
+            cp.freeConnection(conn);
+         }
+        catch(SQLException ex){
+            ex.printStackTrace();
+        }
+         
+        return result;
+    }
+     
+     //get the faculty email from the facultyUsername
+    public String getFacultyEmail(String facultyUsername){
+        String result = "";
+        String sql = "select email from ma_faculty where username =?;";
+        ConnectionPool cp = ConnectionPool.getInstance();
+        
+        
+         try{
+            Connection conn = cp.getConnection();
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, facultyUsername);
+            ResultSet rs = st.executeQuery();
+            
+            while (rs.next()){
+                result = rs.getString(1);
+            }
+            
+            st.close();
+            rs.close();
+            cp.freeConnection(conn);
+         }
+        catch(SQLException ex){
+            ex.printStackTrace();
+        }
+         
+        return result;
+    }
+    
+}
