e3ce4d0455db45745336bd83c7fd356051fec7bf
massvm <massvm@massvm-VirtualBox>
2022-03-19 11:37:15 -0600

    Admin Commits/Builds

--- src/java/Controller/Navigation/SiteNavigationAdmin.java
+++ src/java/Controller/Navigation/SiteNavigationAdmin.java
@@ -36,75 +36,92 @@ public class SiteNavigationAdmin extends HttpServlet {
             throws ServletException, IOException {
         //get session object admin
         HttpSession session = request.getSession();
-        Admin admin = (Admin)session.getAttribute("admin");
-        
+        Admin admin = (Admin) session.getAttribute("admin");
+
         //obtaining values from request scope
         String nav = request.getParameter("nav");
         String logout = request.getParameter("logout");
         String op = request.getParameter("op");
-        
-         if(logout!=null&&!logout.equals("")){
-          session.invalidate();
-          request.setAttribute("message", "User successfully logged out.");
-          request.getRequestDispatcher("/WEB-INF/student/loginV2.jsp").forward(request, response); 
-        }
-        else if(nav!=null&&!nav.equals("")){
-            
-            if(nav.equals("users")){
-                
+
+        if (logout != null && !logout.equals("")) {
+            session.invalidate();
+            request.setAttribute("message", "User successfully logged out.");
+            request.getRequestDispatcher("/WEB-INF/student/loginV2.jsp").forward(request, response);
+        } else if (nav != null && !nav.equals("")) {
+
+            if (nav.equals("users")) {
+
                 //op perameter added for pages with multiple ops
                 //this is only one option for implementation it could value you check and assign 
                 //but if the value is not null then it just needs the op and forward it to the 
                 //page with the op value I just don't know if it will transfer
-                if(op!=null&&!op.equals("")){
-                    request.setAttribute("op", op); 
+                if (op != null && !op.equals("")) {
+                    String usertype = "Undefined";
+                    if (op.equals("1")) {
+
+                        request.setAttribute("usertype", "Student");
+                        request.getRequestDispatcher("/WEB-INF/admin/AdminUsers.jsp").forward(request, response);
+                    } else if (op.equals("2")) {
+
+                        
+                        request.setAttribute("usertype", "Faculty");
+                        request.getRequestDispatcher("/WEB-INF/admin/AdminUsers.jsp").forward(request, response);
+                    } else if (op.equals("3")) {
+                        
+                        request.setAttribute("usertype", "Admin");
+                        request.getRequestDispatcher("/WEB-INF/admin/AdminUsers.jsp").forward(request, response);
+                    } else if (op.equals("4")) {
+
+                        request.setAttribute("usertype", usertype);
+                        request.getRequestDispatcher("/WEB-INF/admin/AdminUserCreate.jsp").forward(request, response);
+                    } else {
+                        request.setAttribute("usertype", usertype);
+                        request.getRequestDispatcher("/WEB-INF/admin/AdminUsers.jsp").forward(request, response);
+                    }
+                    
+                } else {
+                    request.getRequestDispatcher("/WEB-INF/admin/AdminUsers.jsp").forward(request, response);
                 }
-                
-                
-                request.getRequestDispatcher("/WEB-INF/admin/AdminUsers.jsp").forward(request, response);
-            }
+
+            } 
             
-            else if(nav.equals("assignments")){
+            
+            else if (nav.equals("assignments")) {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminAssignments.jsp").forward(request, response);
-            }
+            } 
             
-            else if(nav.equals("modules")){
+            else if (nav.equals("modules")) {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminModules.jsp").forward(request, response);
-            }
+            } 
             
-            else if(nav.equals("courses")){
+            
+            else if (nav.equals("courses")) {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminCourses.jsp").forward(request, response);
-            }
+            } 
             
-            else if(nav.equals("cohort")){
+            
+            else if (nav.equals("cohort")) {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminCohort.jsp").forward(request, response);
-            }
+            } 
             
-            else if(nav.equals("announcements")){
+            
+            else if (nav.equals("announcements")) {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminAnnoucements.jsp").forward(request, response);
-            
-            }
-            else if(nav.equals("adminreport")){
+
+            } else if (nav.equals("adminreport")) {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminReports.jsp").forward(request, response);
-            
-            }
-            
-            else if(nav.equals("adminarchive")){
+
+            } else if (nav.equals("adminarchive")) {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminArchive.jsp").forward(request, response);
-            }
-            
-            else if(nav.equals("adminbr")){
+            } else if (nav.equals("adminbr")) {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminRecover.jsp").forward(request, response);
-            }
-            
-            else{
+            } else {
                 request.getRequestDispatcher("/WEB-INF/admin/AdminLanding.jsp").forward(request, response);
             }
-            
-        }
-        //first time the admin is logging in
-        else{
-            
+
+        } //first time the admin is logging in
+        else {
+
             request.getRequestDispatcher("/WEB-INF/admin/AdminLanding.jsp").forward(request, response);
         }
     }
