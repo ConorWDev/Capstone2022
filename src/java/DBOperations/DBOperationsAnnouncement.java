2d05d6830342cf51336d1dd96d8b4d6c04a3a5e7
RyanRyan-create <ryan.checora@gmail.com>
2022-03-22 18:03:37 -0600

    changes to db

--- src/java/DBOperations/DBOperationsAnnouncement.java
+++ src/java/DBOperations/DBOperationsAnnouncement.java
@@ -29,19 +29,20 @@ public class DBOperationsAnnouncement {
             st.setString(1, cohortId);
             ResultSet rs = st.executeQuery();
             
-//          String announcementId ="";
+          String announcementId ="";
             String startDate ="";
             String endDate = "";
             String text = "";
             String isVisible = "";
             
             while (rs.next()) {
+                announcementId = rs.getString(1);
                 startDate = rs.getString(3);
                 endDate = rs.getString(4);
                 text = rs.getString(5);
                 isVisible = rs.getString(6);
                 
-                Announcement announcement = new Announcement(cohortId,startDate,endDate,text,isVisible);
+                Announcement announcement = new Announcement(announcementId, cohortId,startDate,endDate,text,isVisible);
                 cohortAnnouncements.add(announcement);
                 
             }
@@ -166,6 +167,83 @@ public class DBOperationsAnnouncement {
             PreparedStatement st = conn.prepareStatement(sql);
             st.setString(1, newText);
             st.setString(2, courseAnnouncementID);
+            
+            int rowsAffected = st.executeUpdate();
+            
+            result = (rowsAffected > 0);
+            
+            st.close();
+            cp.freeConnection(conn);
+        } catch(Exception e){}
+            
+        return result;
+    }
+    
+     public boolean createCohortAnnouncement(String cohortID, String text){
+        boolean result = false;
+        //currently insert dummy values for start/end date
+        String sql = "insert into ma_announcement (cohort_id,start_time,end_time,text) values (?,'2022-02-10','2022-02-10',?);";
+       
+        ConnectionPool cp = ConnectionPool.getInstance();
+        
+        try {
+            Connection conn = cp.getConnection();
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, cohortID);
+            st.setString(2, text);
+            
+            int rowsAffected = st.executeUpdate();
+            
+            result = (rowsAffected > 0);
+            
+            st.close();
+            cp.freeConnection(conn);
+        } catch(Exception e){}
+            return result;
+    }
+     
+    public Announcement getCohortAnnouncement (String announcementID){
+        Announcement announcement = null;
+        String sql = "select * from ma_announcement where announcement_id = ?;";
+        ConnectionPool cp = ConnectionPool.getInstance();
+        
+        try {
+            Connection conn = cp.getConnection();
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, announcementID);
+            ResultSet rs = st.executeQuery();
+            
+            while (rs.next()) {
+                
+                String cohort_id = rs.getString(2);
+                String start_time = rs.getString(3);
+                String end_time = rs.getString(4);
+                String text = rs.getString(5);
+                String isVisible = rs.getString(6);
+                
+                announcement = new Announcement(announcementID,cohort_id,start_time,end_time,text,isVisible);
+            }
+            
+            rs.close();
+            st.close();
+            cp.freeConnection(conn);
+        } catch(Exception e){}    
+        
+        
+        return announcement;
+    }
+    
+    //method used when editing courseAnnouncements. Input courseAnnouncementID and new text that will replace it
+    public boolean editCohortAnnouncement (String cohortAnnouncementID, String newText){
+        boolean result = false;
+        String sql = "update ma_announcement set text = ? where announcement_id = ?;";
+         ConnectionPool cp = ConnectionPool.getInstance();
+        
+        try {
+            Connection conn = cp.getConnection();
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, newText);
+            st.setString(2, cohortAnnouncementID);
             
             int rowsAffected = st.executeUpdate();
             
