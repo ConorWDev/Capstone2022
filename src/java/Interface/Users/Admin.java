562c19ca7c9df1bbd38905ef1ddb315275173418
RyanRyan-create <ryan.checora@gmail.com>
2022-03-23 18:55:55 -0600

    changes to admin

--- src/java/Interface/Users/Admin.java
+++ src/java/Interface/Users/Admin.java
@@ -20,10 +20,43 @@ public class Admin {
     //opting to store full name within one field
     private String fullName = "";
     
+    //for admin form, need separate first/middle/last name
+    private String firstName = "";
+    private String middleName = "";
+    private String lastName = "";
+    
     public Admin(String userID){
         this.userID = userID;
         this.fullName = dbOps.getAdminName(userID);
         this.email = dbOps.getAdminEmail(userID); 
+        
+        this.firstName = dbOps.getFirstname(userID);
+        this.middleName = dbOps.getMiddlename(userID);
+        this.lastName = dbOps.getLastname(userID);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public DBOperationsAdmin getDbOps() {
